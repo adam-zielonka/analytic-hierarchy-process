dd4f9a68c79a66f0dc5c584b46015311c99e6fe9
Adam Zielonka <adam.zielonka@outlook.com>
2017-04-11 20:48:06 +0200

    Saaty 4.1

--- Saaty/FormMain.Designer.cs
+++ Saaty/FormMain.Designer.cs
@@ -28,12 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             this.tabControlMain = new System.Windows.Forms.TabControl();
             this.tabPageStart = new System.Windows.Forms.TabPage();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonNewProject = new System.Windows.Forms.Button();
+            this.richTextBoxStart = new System.Windows.Forms.RichTextBox();
+            this.tabPageStep1 = new System.Windows.Forms.TabPage();
+            this.tabPageStep2 = new System.Windows.Forms.TabPage();
+            this.tabPageStep3 = new System.Windows.Forms.TabPage();
+            this.tabPageStep4 = new System.Windows.Forms.TabPage();
             this.tabPageResults = new System.Windows.Forms.TabPage();
             this.buttonBack = new System.Windows.Forms.Button();
             this.buttonNext = new System.Windows.Forms.Button();
@@ -42,19 +46,20 @@
             this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.tabControlMain.SuspendLayout();
             this.tabPageStart.SuspendLayout();
             this.menuStripMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControlMain
             // 
             this.tabControlMain.Controls.Add(this.tabPageStart);
-            this.tabControlMain.Controls.Add(this.tabPage1);
-            this.tabControlMain.Controls.Add(this.tabPage2);
-            this.tabControlMain.Controls.Add(this.tabPage3);
-            this.tabControlMain.Controls.Add(this.tabPage4);
+            this.tabControlMain.Controls.Add(this.tabPageStep1);
+            this.tabControlMain.Controls.Add(this.tabPageStep2);
+            this.tabControlMain.Controls.Add(this.tabPageStep3);
+            this.tabControlMain.Controls.Add(this.tabPageStep4);
             this.tabControlMain.Controls.Add(this.tabPageResults);
             this.tabControlMain.Location = new System.Drawing.Point(12, 27);
             this.tabControlMain.Name = "tabControlMain";
@@ -64,7 +69,10 @@
             // 
             // tabPageStart
             // 
-            this.tabPageStart.Controls.Add(this.richTextBox1);
+            this.tabPageStart.Controls.Add(this.pictureBox1);
+            this.tabPageStart.Controls.Add(this.button1);
+            this.tabPageStart.Controls.Add(this.buttonNewProject);
+            this.tabPageStart.Controls.Add(this.richTextBoxStart);
             this.tabPageStart.Location = new System.Drawing.Point(4, 22);
             this.tabPageStart.Name = "tabPageStart";
             this.tabPageStart.Padding = new System.Windows.Forms.Padding(3);
@@ -73,45 +81,77 @@
             this.tabPageStart.Text = "Start";
             this.tabPageStart.UseVisualStyleBackColor = true;
             // 
-            // tabPage1
+            // button1
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(486, 316);
-            this.tabPage1.TabIndex = 2;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.button1.Location = new System.Drawing.Point(336, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Otwórz istniejący projekt";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // buttonNewProject
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(486, 316);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.buttonNewProject.Location = new System.Drawing.Point(175, 145);
+            this.buttonNewProject.Name = "buttonNewProject";
+            this.buttonNewProject.Size = new System.Drawing.Size(144, 23);
+            this.buttonNewProject.TabIndex = 4;
+            this.buttonNewProject.Text = "Utwórz nowy projekt";
+            this.buttonNewProject.UseVisualStyleBackColor = true;
             // 
-            // tabPage3
+            // richTextBoxStart
             // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(486, 316);
-            this.tabPage3.TabIndex = 3;
-            this.tabPage3.Text = "tabPage3";
-            this.tabPage3.UseVisualStyleBackColor = true;
+            this.richTextBoxStart.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.richTextBoxStart.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBoxStart.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.richTextBoxStart.Location = new System.Drawing.Point(175, 6);
+            this.richTextBoxStart.Name = "richTextBoxStart";
+            this.richTextBoxStart.ReadOnly = true;
+            this.richTextBoxStart.ShortcutsEnabled = false;
+            this.richTextBoxStart.Size = new System.Drawing.Size(305, 133);
+            this.richTextBoxStart.TabIndex = 0;
+            this.richTextBoxStart.TabStop = false;
+            this.richTextBoxStart.Text = resources.GetString("richTextBoxStart.Text");
             // 
-            // tabPage4
+            // tabPageStep1
             // 
-            this.tabPage4.Location = new System.Drawing.Point(4, 22);
-            this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage4.Size = new System.Drawing.Size(486, 316);
-            this.tabPage4.TabIndex = 4;
-            this.tabPage4.Text = "tabPage4";
-            this.tabPage4.UseVisualStyleBackColor = true;
+            this.tabPageStep1.Location = new System.Drawing.Point(4, 22);
+            this.tabPageStep1.Name = "tabPageStep1";
+            this.tabPageStep1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageStep1.Size = new System.Drawing.Size(486, 316);
+            this.tabPageStep1.TabIndex = 2;
+            this.tabPageStep1.Text = "Krok 1.";
+            this.tabPageStep1.UseVisualStyleBackColor = true;
+            // 
+            // tabPageStep2
+            // 
+            this.tabPageStep2.Location = new System.Drawing.Point(4, 22);
+            this.tabPageStep2.Name = "tabPageStep2";
+            this.tabPageStep2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageStep2.Size = new System.Drawing.Size(486, 316);
+            this.tabPageStep2.TabIndex = 1;
+            this.tabPageStep2.Text = "Krok 2.";
+            this.tabPageStep2.UseVisualStyleBackColor = true;
+            // 
+            // tabPageStep3
+            // 
+            this.tabPageStep3.Location = new System.Drawing.Point(4, 22);
+            this.tabPageStep3.Name = "tabPageStep3";
+            this.tabPageStep3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageStep3.Size = new System.Drawing.Size(486, 316);
+            this.tabPageStep3.TabIndex = 3;
+            this.tabPageStep3.Text = "Krok 3.";
+            this.tabPageStep3.UseVisualStyleBackColor = true;
+            // 
+            // tabPageStep4
+            // 
+            this.tabPageStep4.Location = new System.Drawing.Point(4, 22);
+            this.tabPageStep4.Name = "tabPageStep4";
+            this.tabPageStep4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageStep4.Size = new System.Drawing.Size(486, 316);
+            this.tabPageStep4.TabIndex = 4;
+            this.tabPageStep4.Text = "Krok 4.";
+            this.tabPageStep4.UseVisualStyleBackColor = true;
             // 
             // tabPageResults
             // 
@@ -186,17 +226,15 @@
             this.saveToolStripMenuItem.Text = "Zapisz";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
-            // richTextBox1
+            // pictureBox1
             // 
-            this.richTextBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.richTextBox1.Location = new System.Drawing.Point(116, 51);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(253, 49);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "Witam w najlepszym programie we wszechświecie. \nProgram pozwoli ci podjąć decyzje" +
-    " wykorzystując analizę hierarchii Satty’ego. ";
+            this.pictureBox1.Image = global::Saaty.Properties.Resources.p1;
+            this.pictureBox1.Location = new System.Drawing.Point(7, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(162, 304);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
             // 
             // FormMain
             // 
@@ -214,6 +252,7 @@
             this.tabPageStart.ResumeLayout(false);
             this.menuStripMain.ResumeLayout(false);
             this.menuStripMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -223,7 +262,7 @@
 
         private System.Windows.Forms.TabControl tabControlMain;
         private System.Windows.Forms.TabPage tabPageStart;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPageStep2;
         private System.Windows.Forms.Button buttonBack;
         private System.Windows.Forms.Button buttonNext;
         private System.Windows.Forms.MenuStrip menuStripMain;
@@ -231,11 +270,14 @@
         private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage3;
-        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPageStep1;
+        private System.Windows.Forms.TabPage tabPageStep3;
+        private System.Windows.Forms.TabPage tabPageStep4;
         private System.Windows.Forms.TabPage tabPageResults;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBoxStart;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonNewProject;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
